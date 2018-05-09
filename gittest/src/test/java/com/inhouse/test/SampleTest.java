07ecc6f0e449cbf46935f1fe903860560aeef0ef
TYSS <TYSS@TYSS-4F-076>
2018-05-09 15:38:48 +0530

    hi from loacl

--- gittest/src/test/java/com/inhouse/test/SampleTest.java
+++ gittest/src/test/java/com/inhouse/test/SampleTest.java
@@ -7,6 +7,7 @@ public class SampleTest {
 	@Test
 	public void createAccout(){
 		System.out.println("hello");
-		
+		System.out.println("hi from local");
+		System.out.println("hi from git");
 	}
 }
